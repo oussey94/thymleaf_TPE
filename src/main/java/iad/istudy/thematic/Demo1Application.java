4eb66d6340c7e78c3bffeae35a2a8821d004d615
Mor SECK <mor.seck@bushra.sn>
2021-05-06 12:00:49 +0000

    ✨ Get Theme by code method
    [x] Add findByCode method on ThemeRepository
    [x] Add Data Test

--- src/main/java/iad/istudy/thematic/Demo1Application.java
+++ src/main/java/iad/istudy/thematic/Demo1Application.java
@@ -14,18 +14,38 @@ import iad.istudy.thematic.repositories.ThemeRepository;
 public class Demo1Application implements CommandLineRunner{
 	@Autowired
 	private ThemeRepository themeRepo;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
 
+	public Theme addThemeTest(String code, String name, String description, int weight, String code_parent) {
+		
+		Theme theme = new Theme();
+		
+		theme.setCode(code);
+		theme.setName(name);
+		theme.setDescription(description);
+		theme.setWeight(weight);
+		
+		if(code_parent != null)
+			theme.setParent(themeRepo.findByCode(code_parent));
+	
+		return theme;
+	}
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		/*themeRepo.save(new Theme("hfhhff", new ArrayList<>()));
-		themeRepo.save(new Theme("dddd", new ArrayList<>()));
-		themeRepo.save(new Theme("ddd", new ArrayList<>()));
-		*/
+		
+		themeRepo.save(addThemeTest("FEATURE", "Caractéristiques", "Caractéristiques", 1, null));
+		themeRepo.save(addThemeTest("DYNAMIC", "Dynamique territoriale", "Dynamique territoriale", 2, "FEATURE"));
+		themeRepo.save(addThemeTest("THERPRR", "THERPRR territoriale", "THERPRR territoriale", 5, "DYNAMIC"));
+		
+	
+		 
+		
 	}
 
 }
--- src/main/java/iad/istudy/thematic/repositories/ThemeRepository.java
+++ src/main/java/iad/istudy/thematic/repositories/ThemeRepository.java
@@ -1,12 +1,13 @@
 package iad.istudy.thematic.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import iad.istudy.thematic.entities.Theme;
 
 @Repository
 public interface ThemeRepository extends JpaRepository<Theme, Long>{
+
+	public Theme findByCode(String code);
 
 }
